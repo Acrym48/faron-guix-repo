fb2012532a82c0cff6ec46c196748cc62c81c93b
faron <faron@local>
2026-09-01 23:58:41 +0500

    add polymc: Minecraft launcher (PolyMC 7.1)

---
+++ faron/packages/polymc.scm
@@ -0,0 +1,62 @@
+(define-module (faron packages polymc)
+  #:use-module (guix packages)
+  #:use-module (guix gexp)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system cmake)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages audio)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages cpp)
+  #:use-module (gnu packages gl)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages java)
+  #:use-module (gnu packages kde-frameworks)
+  #:use-module (gnu packages man)
+  #:use-module (gnu packages qt)
+  #:export (polymc))
+
+(define-public polymc
+  (package
+    (name "polymc")
+    (version "7.1")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/PolyMC/PolyMC")
+             (commit "9b01b80bfd43768c478ad6a0bdf532bc51635973")
+             ;; Сабмодули (libnbtplusplus, quazip, tomlplusplus,
+             ;; ghc-filesystem) нужны для сборки.
+             (recursive? #t)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "05zr0ib3a78yrhh2wnc3adidw17za89fd56c0pyq8gkrf6kakak8"))))
+    (build-system cmake-build-system)
+    (arguments
+     (list
+      #:configure-flags
+      #~(list "-DLauncher_APP_BINARY_NAME=polymc"
+              "-DLauncher_QT_VERSION_MAJOR=6")))
+    (native-inputs
+     (list extra-cmake-modules scdoc (list openjdk21 "jdk")))
+    (inputs
+     (list qtbase
+           qt5compat
+           qtsvg
+           qtimageformats
+           qtcharts
+           quazip
+           tomlplusplus
+           zlib
+           glfw
+           openal
+           mesa
+           hicolor-icon-theme
+           openjdk21))
+    (home-page "https://github.com/PolyMC/PolyMC")
+    (synopsis "Free, open source launcher for Minecraft")
+    (description
+     "PolyMC is a custom launcher for Minecraft that allows you to easily
+manage multiple installations of Minecraft at once.  It provides a rich
+user interface for managing instances, mods, resource packs and more.")
+    (license license:gpl3)))
